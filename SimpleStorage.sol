d1a7471fe3b6f9b13b66e4dce57968eeac7165bd
bayorwor <bayorwor60@gmail.com>
2024-10-23 18:05:07 +0000

    something is wrong with my ....

---
+++ SimpleStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25; //stating solidity verion
+
+contract SimpleStorage{
+    // favoriteNumber will initialize to zero
+    uint256 myFavoriteNumber; // default to 0 as value
+    uint256[] listOfFavoriteNumbers;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber=_favoriteNumber;
+    }
+
+    // view, pure
+    function retrieve() public view returns(uint256){
+        return myFavoriteNumber;
+    }
+
+    function addNewPerson(string memory _name, uint256 _favoriteNumber) public {
+        Person memory newPerson= Person({favoriteNumber: _favoriteNumber, name: _name});
+        listOfPeople.push(newPerson);
+    }
+}
